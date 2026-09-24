db5769b9079943f53e33b848b6684d240bb9c177
aaditya1606 <aadityashakya.tech@yahoo.com>
2026-09-24 21:39:34 +0530

    TWO STRINGS ARE REVERSE OF EACH OTHER OR NOT

--- two_strings_reverse_or_not.c
+++ two_strings_reverse_or_not.c
@@ -6,7 +6,48 @@ AUTHOR- AADITYA SHAKYA
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-int main(){
-    
+int main()
+{
+    int size1, size2;
+    printf("ENTER THE SIZE OF THE STRINGs: ");
+    scanf("%d", &size1);
+    size2 = size1;
+    while (size1 <= 1)
+    {
+        printf("ENTER A VALID SIZE!! RE-ENTER: ");
+        scanf("%d", &size1);
+    }
+    char strings1[size1];
+    char strings2[size2];
+    printf("ENTER THE FIRST STRING: ");
+    scanf("%s", &strings1);
+    printf("ENTER THE SECOND STRING: ");
+    scanf("%s", &strings2);
+    int i = 0;
+    int j = strlen(strings2) - 1;
+    int reverse = 1;
+    while (strings1[i] != '\0' && j != 0)
+    {
+        if (strings1[i] != strings2[j])
+        {
+            reverse = 0;
+            break;
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    if (reverse == 1)
+    {
+        printf("STRINGS ARE REVERSE OF EACH OTHER!!");
+    }
+    else
+    {
+        printf("STRINGS ARE NOT REVERSE OF EACH OTHER!!");
+    }
+    return 0;
 }
